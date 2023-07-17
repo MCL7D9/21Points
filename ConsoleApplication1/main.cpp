d945a28c53d0da8c0901208333b9962abaf136ff
MCL7D9 <mcl7d9@outlook.com>
2023-07-17 17:47:23 +0800

    Happy Happy Happy

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -4,40 +4,34 @@
 #include <cmath>
 #include <ctime>
 using namespace std;
+#define USI unsigned short int
 
-int* shuffle() {
-    int poker[52] = { 0 }, temp, rann;
-
-    for (int i = 0; i < 13; i++) {
-        poker[i] = i + 1;
-    }
-    for (int i = 0; i < 3; i++) {
+USI &shuffle() {
+    USI poker[52] = { 0 }, temp, rann, & ref_poker = *poker;
+    for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 13; j++) {
-            poker[(i + 1) * 13 + j] = poker[j];
+            poker[i * 13 + j] = j + 1;
         }
     }
     for (int i = 0; i < 52; i++) {
-        srand(time(NULL));
+        srand(time(nullptr));
         rann = rand() % (52 - i);
         temp = poker[i];
         poker[i] = poker[rann];
         poker[rann] = temp;
     }
-    return poker;
+    return ref_poker;
 }
 
-int decide() {
-}
 
 class player {
-
 private:
     int cards[20] = { 0 }, top = 0,sum=0;
 
 public:
-    void give_card(int* poker_set, int *poker_top) {
+    void give_card(USI *poker_set, USI *poker_top) {
         cards[top] = poker_set[*poker_top];
-        sum += *poker_top;
+        sum -= *poker_top;
         *poker_top += 1;
         top += 1;
     }
@@ -49,12 +43,12 @@ public:
         }
         cout << "==========";
     }
+    player(USI* a, int b, string name);
 
-    player(int*, int, string);
 };
 
-player::player(int* a,int b,string name="AI") {
-    int* poker = a;
+player::player(USI* a,int b,string name="AI") {
+    USI* poker = a;
     int poker_top=b;
     string id = name;
 }
@@ -67,10 +61,10 @@ int main()
     cout << "Please Enter Your Name\n";
     cin >> name;
     cout << "==========Game Start==========\n";
-    int *poker_set = shuffle(), poker_top = 51;
+    USI *poker_set = &shuffle(), poker_top = 51;
     player P1(poker_set,poker_top,name), P2(poker_set, poker_top);
     for (int i = 0; i < 2; i++) {
-        P1.give_card(poker_set,&poker_top);
+        P1.give_card(poker_set, &poker_top);
         P2.give_card(poker_set, &poker_top);
     }
     P1.echo_card(name);
