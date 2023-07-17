4cfa2cde599815d6f4dbcd0825d2f6be730093c7
MCL7D9 <mcl7d9@outlook.com>
2023-07-17 18:25:49 +0800

    Nice 馬飛飛

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -5,6 +5,8 @@
 #include <ctime>
 using namespace std;
 #define USI unsigned short int
+USI* poker_set, poker_top = 51;
+string name;
 
 USI &shuffle() {
     USI poker[52] = { 0 }, temp, rann, & ref_poker = *poker;
@@ -23,52 +25,58 @@ USI &shuffle() {
     return ref_poker;
 }
 
-
 class player {
 private:
     int cards[20] = { 0 }, top = 0,sum=0;
 
 public:
-    void give_card(USI *poker_set, USI *poker_top) {
-        cards[top] = poker_set[*poker_top];
-        sum -= *poker_top;
-        *poker_top += 1;
+    void give_card() {
+        cards[top] = poker_set[poker_top];
+        poker_top -= 1;
         top += 1;
     }
 
-    void echo_card(string name) {
+    void echo_card() {
         cout << "Player " << name << "\'s card set are:\n ";
         for (int i = 0; i < top; i++) {
             cout << "[" << cards[i] << "] ";
         }
-        cout << "==========";
+        cout << "====================\n";
     }
-    player(USI* a, int b, string name);
 
 };
 
-player::player(USI* a,int b,string name="AI") {
-    USI* poker = a;
-    int poker_top=b;
-    string id = name;
+int round_ctrl(player *Player,player *AI) {
+    char p1_choice=0, p2_choice=0;
+    while (p1_choice != 78){
+        cout << "Do You Want To Add A Card?\n Yes[Y] No[N] Information[I]\n";
+        cin.get(p1_choice);
+        switch (p1_choice) {
+        case 59:
+            Player->give_card();
+        case 78:
+        case 73:
+        default:
+            break;
+        }
+    }
+    return 0;
 }
-
 
 int main()
 { 
-    string name;
     cout << "Welcome To The Game 21 Points!\n";
     cout << "Please Enter Your Name\n";
     cin >> name;
     cout << "==========Game Start==========\n";
-    USI *poker_set = &shuffle(), poker_top = 51;
-    player P1(poker_set,poker_top,name), P2(poker_set, poker_top);
+    poker_set = &shuffle();
+    player P1, P2;
     for (int i = 0; i < 2; i++) {
-        P1.give_card(poker_set, &poker_top);
-        P2.give_card(poker_set, &poker_top);
+        P1.give_card();
+        P2.give_card();
     }
-    P1.echo_card(name);
-
+    P1.echo_card();
+    round_ctrl(&P1,&P2);
     system("pause");
     return 0;
 }
